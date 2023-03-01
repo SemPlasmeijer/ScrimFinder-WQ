200d59d2bb4c89182e66c14fe163c3bc7345e3c1
Sem Plasmeijer <plasmeijer.sem@gmail.com>
2023-03-01 15:06:45 +0100

    docker

--- src/main/java/com/scrimfinderwq/scrimFinderWQ/ScrimFinderWqApplication.java
+++ src/main/java/com/scrimfinderwq/scrimFinderWQ/ScrimFinderWqApplication.java
@@ -1,13 +1,26 @@
 package com.scrimfinderwq.scrimFinderWQ;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.scrimfinderwq.scrimFinderWQ.models.workqueues.Producer;
+
 @SpringBootApplication
-public class ScrimFinderWqApplication {
+public class ScrimFinderWqApplication implements CommandLineRunner {
+
+	@Autowired
+	private Producer producer;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ScrimFinderWqApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		for (int i = 10000; i < 10010; i++) {
+			producer.sendMsg(i);
+		}
+	}
 }
---
+++ src/main/java/com/scrimfinderwq/scrimFinderWQ/models/workqueues/Producer.java
@@ -0,0 +1,19 @@
+package com.scrimfinderwq.scrimFinderWQ.models.workqueues;
+
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class Producer {
+    @Autowired
+    private Queue queue;
+
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
+
+    public void sendMsg(final Integer number) {
+        rabbitTemplate.convertAndSend(queue.getName(), number);
+        System.out.println("Sent: " + number);
+    }
+
+}
