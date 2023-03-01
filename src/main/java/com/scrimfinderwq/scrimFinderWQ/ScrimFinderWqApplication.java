c92e73833bf76681d3e05b5080388bc21efb33cf
Sem Plasmeijer <plasmeijer.sem@gmail.com>
2023-03-01 11:25:11 +0100

    base spring

---
+++ src/main/java/com/scrimfinderwq/scrimFinderWQ/ScrimFinderWqApplication.java
@@ -0,0 +1,13 @@
+package com.scrimfinderwq.scrimFinderWQ;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScrimFinderWqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScrimFinderWqApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/scrimfinderwq/scrimFinderWQ/ScrimFinderWqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scrimfinderwq.scrimFinderWQ;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScrimFinderWqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
