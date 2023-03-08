3294855d9687e80e3260a3244947a0052b65c57c
SemPlasmeijer <plasmeijer.sem@gmail.com>
2023-03-08 14:56:18 +0100

    docker sync

--- src/main/java/com/scrimfinderwq/scrimFinderWQ/ScrimFinderWqApplication.java
+++ src/main/java/com/scrimfinderwq/scrimFinderWQ/ScrimFinderWqApplication.java
@@ -1,26 +1,17 @@
 package com.scrimfinderwq.scrimFinderWQ;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.scrimfinderwq.scrimFinderWQ.models.workqueues.Producer;
 
+@EnableRabbit
 @SpringBootApplication
-public class ScrimFinderWqApplication implements CommandLineRunner {
+public class ScrimFinderWqApplication{
 
-	@Autowired
-	private Producer producer;
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(ScrimFinderWqApplication.class, args);
-	}
-
-	@Override
-	public void run(String... args) throws Exception {
-		for (int i = 10000; i < 10010; i++) {
-			producer.sendMsg(i);
-		}
 	}
 }
