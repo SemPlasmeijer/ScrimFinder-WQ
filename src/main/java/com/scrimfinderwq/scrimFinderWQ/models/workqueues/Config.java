59d8bccc7093488c2d5928cf49247a22dcbea5b2
SemPlasmeijer <plasmeijer.sem@gmail.com>
2023-03-15 15:49:23 +0100

    config

--- src/main/java/com/scrimfinderwq/scrimFinderWQ/models/workqueues/Config.java
+++ src/main/java/com/scrimfinderwq/scrimFinderWQ/models/workqueues/Config.java
@@ -1,18 +1,50 @@
 package com.scrimfinderwq.scrimFinderWQ.models.workqueues;
 
+import org.springframework.amqp.core.DirectExchange;
 import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.rabbit.listener.ConditionalRejectingErrorHandler;
+import org.springframework.amqp.rabbit.support.ListenerExecutionFailedException;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.util.ErrorHandler;
 
-import com.scrimfinderwq.scrimFinderWQ.models.workqueues.Consumer;
-import com.scrimfinderwq.scrimFinderWQ.models.workqueues.Producer;
-
+@EnableRabbit
 @Configuration
 public class Config {
 
     @Value("${queue.name}")
     private String queueName;
+    @Value("${spring.rabbitmq.template.exchange}")
+    private String exchange;
+    @Value("${spring.rabbitmq.template.routing-key}")
+    private String routingkey;
+    @Value("${spring.rabbitmq.username}")
+    private String username;
+    @Value("${spring.rabbitmq.password}")
+    private String password;
+    @Value("${spring.rabbitmq.host}")
+    private String host;
+    @Value("${spring.rabbitmq.virtualhost}")
+    private String virtualHost;
+    @Value("${spring.rabbitmq.connection-timeout}")
+    private Integer replyTimeout;
+    @Value("${spring.rabbitmq.listener.direct.consumers-per-queue}")
+    private Integer concurrentConsumers;
+    @Value("${spring.rabbitmq.listener.direct.consumers-per-queue}")
+    private Integer maxConcurrentConsumers;
 
     @Bean
     public Queue queue() {
@@ -32,6 +64,70 @@ public class Config {
     @Bean
     public Consumer consumer2() {
         return new Consumer(2);
+    }
+
+    @Bean
+    public DirectExchange exchange() {
+        return new DirectExchange(exchange);
+    }
+    @Bean
+    public Binding binding(Queue queue, DirectExchange exchange) {
+        return BindingBuilder.bind(queue).to(exchange).with(routingkey);
+    }
+    @Bean
+    public MessageConverter jsonMessageConverter() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        return new Jackson2JsonMessageConverter(objectMapper);
+    }
+    @Bean
+    public ConnectionFactory connectionFactory() {
+        CachingConnectionFactory connectionFactory = new CachingConnectionFactory();
+        connectionFactory.setVirtualHost(virtualHost);
+        connectionFactory.setHost(host);
+        connectionFactory.setUsername(username);
+        connectionFactory.setPassword(password);
+        return connectionFactory;
+    }
+    @Bean
+    public AmqpTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
+        final RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setDefaultReceiveQueue(queueName);
+        rabbitTemplate.setMessageConverter(jsonMessageConverter());
+        rabbitTemplate.setReplyAddress(queue().getName());
+        rabbitTemplate.setReplyTimeout(replyTimeout);
+        rabbitTemplate.setUseDirectReplyToContainer(false);
+        return rabbitTemplate;
+    }
+    @Bean
+    public AmqpAdmin amqpAdmin() {
+        return new RabbitAdmin(connectionFactory());
+    }
+    @Bean
+    public SimpleRabbitListenerContainerFactory rabbitListenerContainerFactory() {
+        final SimpleRabbitListenerContainerFactory factory = new SimpleRabbitListenerContainerFactory();
+        factory.setConnectionFactory(connectionFactory());
+        factory.setMessageConverter(jsonMessageConverter());
+        factory.setConcurrentConsumers(concurrentConsumers);
+        factory.setMaxConcurrentConsumers(maxConcurrentConsumers);
+        factory.setErrorHandler(errorHandler());
+        return factory;
+    }
+    @Bean
+    public ErrorHandler errorHandler() {
+        return new ConditionalRejectingErrorHandler(new MyFatalExceptionStrategy());
+    }
+    public static class MyFatalExceptionStrategy extends ConditionalRejectingErrorHandler.DefaultExceptionStrategy {
+        private final Logger logger = LogManager.getLogger(getClass());
+        @Override
+        public boolean isFatal(Throwable t) {
+            if (t instanceof ListenerExecutionFailedException) {
+                ListenerExecutionFailedException lefe = (ListenerExecutionFailedException) t;
+                logger.error("Failed to process inbound message from queue "
+                        + lefe.getFailedMessage().getMessageProperties().getConsumerQueue()
+                        + "; failed message: " + lefe.getFailedMessage(), t);
+            }
+            return super.isFatal(t);
+        }
     }
 
 }
